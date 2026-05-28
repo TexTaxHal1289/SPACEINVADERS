c93f2765adb81603c57bcf53d921afa56331cbfb
TexTaxHal1289 <Texashalunen@gmail.com>
2026-05-28 09:31:58 +0200

    Add project files.

---
+++ SPACEINVADERS/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SPACEINVADERS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(321, 395);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(285, 43);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.form1_keyup);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SPACEINVADERS/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace SPACEINVADERS
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int riktning = 0;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Rectangle rect = new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Left += riktning * 10;
+        }
+        private void form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left) riktning = -1;
+            else if (e.KeyCode == Keys.Right) riktning = 1;
+        }
+
+        private void form1_keyup(object sender, KeyEventArgs e)
+        {
+            riktning = 0;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            timer1.Start();
+
+
+            PictureBox[] enemies = new PictureBox[25];
+            for (int i = 0; i < 25; i++)
+            {
+                enemies[i] = new PictureBox();
+                enemies[i].Image = Properties.Resources.CocaColaLogo;
+                enemies[i].Size = new Size(40, 30);
+                enemies[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                enemies[i].BackColor = Color.Transparent;
+
+                // 5 columns, 5 rows
+                int col = i % 5;
+                int row = i / 5;
+
+                enemies[i].Location = new Point(col * 80 + 40, row * 30 + 40);
+
+                this.Controls.Add(enemies[i]);
+            }
+
+            Rectangle bullet = new Rectangle
+            {
+                Tag = "bullet",
+                Height = 20,
+                Width = 5,
+                fill = Brushes.Red
+                Stroke = Brushes.Red
+
+            };
+    }
+}
+
+
+
+
+
+
+
+
+
+
+}
+
